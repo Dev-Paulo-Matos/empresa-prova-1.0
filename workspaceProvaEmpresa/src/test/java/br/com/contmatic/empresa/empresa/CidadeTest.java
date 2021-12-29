8c7a695972beb119900003b6f6d007191e1e427f
Paulo Machado Fraga Matos <paulo.machado.fraga.matos@gmail.com>
2021-12-29 15:08:06 -0300

    #0000 - Arrumando CidadeTest para o padrao do JUnit 5

--- workspaceProvaEmpresa/src/test/java/br/com/contmatic/empresa/empresa/CidadeTest.java
+++ workspaceProvaEmpresa/src/test/java/br/com/contmatic/empresa/empresa/CidadeTest.java
@@ -1,12 +1,13 @@
 package br.com.contmatic.empresa.empresa;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import br.com.contmatic.empresa.endereco.Cidade;
 
@@ -21,53 +22,68 @@ public class CidadeTest {
 	
 	private Cidade cidadeBefore;
 	
-	@Before
-	public void deve_criar_antes_de_todos_os_teste() {
+	@BeforeEach
+	 void deve_criar_antes_de_todos_os_teste() {
 		cidadeBefore = new Cidade(COD_IBGE);
 	}
 	
 	// C COD
 	
 	@Test
-	public void test_deve_criar_cidade_com_cod_ibge_valido() {
+	void test_deve_criar_cidade_com_cod_ibge_valido() {
 		Cidade c1 = new Cidade(COD_IBGE);
 		
 		assertEquals(COD_IBGE, c1.getCodigoIbge());
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_criar_cidade_com_cod_ibge_vazio() {
-		new Cidade("");
+	@Test
+	void test_nao_deve_criar_cidade_com_cod_ibge_vazio() {
+		assertThrows(IllegalStateException.class,()->{
+			new Cidade("");
+		});
+	
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_nao_deve_criar_cidade_com_cod_ibge_null() {
-		new Cidade(null);
+	@Test
+	void test_nao_deve_criar_cidade_com_cod_ibge_null() {
+		assertThrows(IllegalArgumentException.class,()->{
+			new Cidade(null);
+		});
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_criar_cidade_com_cod_ibge_contendo_caracteres_especiais() {
-		new Cidade("#!@!!");
+	@Test
+	void test_nao_deve_criar_cidade_com_cod_ibge_contendo_caracteres_especiais() {
+		assertThrows(IllegalStateException.class,()->{
+			new Cidade("#!@!!");
+		});
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_criar_cidade_com_cod_ibge_contendo_letras() {
-		new Cidade("Asasa");
+	@Test
+	void test_nao_deve_criar_cidade_com_cod_ibge_contendo_letras() {
+		assertThrows(IllegalStateException.class,()->{
+			new Cidade("Asasa");
+		});
 	}
 		
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_criar_cidade_com_cod_ibge_contendo_espacos() {
-		new Cidade("123 1");
+	@Test
+	void test_nao_deve_criar_cidade_com_cod_ibge_contendo_espacos() {
+		assertThrows(IllegalStateException.class,()->{
+			new Cidade("123 1");
+		});
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_criar_cidade_com_cod_ibge_contendo_mais_digitos_que_o_permitido() {
-		new Cidade("123121");
+	@Test
+	void test_nao_deve_criar_cidade_com_cod_ibge_contendo_mais_digitos_que_o_permitido() {
+		assertThrows(IllegalStateException.class,()->{
+			new Cidade("123121");
+		});
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_criar_cidade_com_cod_ibge_contendo_menos_digitos_que_o_permitido() {
-		new Cidade("1233");
+	@Test
+	void test_nao_deve_criar_cidade_com_cod_ibge_contendo_menos_digitos_que_o_permitido() {
+		assertThrows(IllegalStateException.class,()->{
+			new Cidade("1233");
+		});
 	}
 	
 	
@@ -75,7 +91,7 @@ public class CidadeTest {
 	// SETT
 	
 	@Test
-	public void test_deve_settar_cidade_com_cod_ibge_valido() {
+	void test_deve_settar_cidade_com_cod_ibge_valido() {
 		String codigoIbge = "12345";
 		
 		cidadeBefore.setCodigoIbge(codigoIbge);
@@ -83,148 +99,186 @@ public class CidadeTest {
 		assertEquals(codigoIbge, cidadeBefore.getCodigoIbge());
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_nao_deve_settar_cidade_com_cod_ibge_null() {
-		cidadeBefore.setCodigoIbge(null);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_cidade_com_cod_ibge_vazio() {
-		cidadeBefore.setCodigoIbge("");
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_cidade_com_cod_ibge_contendo_caracteres_especiais() {
-		cidadeBefore.setCodigoIbge("#!@!!");
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_cidade_com_cod_ibge_contendo_letras() {
-		cidadeBefore.setCodigoIbge("Asasa");
-	}
-		
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_cidade_com_cod_ibge_contendo_espacos() {
-		cidadeBefore.setCodigoIbge("123 1");
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_cidade_com_cod_ibge_contendo_mais_digitos_que_o_permitido() {
-		cidadeBefore.setCodigoIbge("123121");
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_cidade_com_cod_ibge_contendo_menos_digitos_que_o_permitido() {
-		cidadeBefore.setCodigoIbge("1233");
+	@Test
+	void test_nao_deve_settar_cidade_com_cod_ibge_null() {
+		assertThrows(IllegalArgumentException.class,()->{
+			cidadeBefore.setCodigoIbge(null);
+		});
 	}
 	
 	@Test
-	public void test_deve_settar_valido_nome_a_cidade() {
+	void test_nao_deve_settar_cidade_com_cod_ibge_vazio() {
+			assertThrows(IllegalStateException.class,()->{
+		cidadeBefore.setCodigoIbge("");
+		});
+	}
+	
+	@Test
+	void test_nao_deve_settar_cidade_com_cod_ibge_contendo_caracteres_especiais() {
+		assertThrows(IllegalStateException.class,()->{
+			cidadeBefore.setCodigoIbge("#!@!!");
+		});
+	}
+	
+	@Test
+	void test_nao_deve_settar_cidade_com_cod_ibge_contendo_letras() {
+		assertThrows(IllegalStateException.class,()->{
+			cidadeBefore.setCodigoIbge("Asasa");
+		});
+	}
+		
+	@Test
+	void test_nao_deve_settar_cidade_com_cod_ibge_contendo_espacos() {
+			assertThrows(IllegalStateException.class,()->{
+		cidadeBefore.setCodigoIbge("123 1");
+		});
+	}
+	
+	@Test
+	void test_nao_deve_settar_cidade_com_cod_ibge_contendo_mais_digitos_que_o_permitido() {
+		assertThrows(IllegalStateException.class,()->{
+			cidadeBefore.setCodigoIbge("123121");
+		});
+	}
+	
+	@Test
+	 void test_nao_deve_settar_cidade_com_cod_ibge_contendo_menos_digitos_que_o_permitido() {
+		assertThrows(IllegalStateException.class,()->{
+			cidadeBefore.setCodigoIbge("1233");
+		});
+	}
+	
+	@Test
+	 void test_deve_settar_valido_nome_a_cidade() {
 		cidadeBefore.setNome(NOME);
 		
 		assertSame(NOME,cidadeBefore.getNome());
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar__nome_vazio() {
+	@Test
+	void test_nao_deve_settar__nome_vazio() {
+		assertThrows(IllegalStateException.class,()->{
 		cidadeBefore.setNome("");
+		});
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_nao_deve_settar__nome_null() {
+	@Test
+	void test_nao_deve_settar__nome_null() {
+		assertThrows(IllegalArgumentException.class,()->{
 		cidadeBefore.setNome(null);
+		});
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar__nome_invalido_contendo_digitos_a_cidade() {
+	@Test
+	void test_nao_deve_settar__nome_invalido_contendo_digitos_a_cidade() {
+		assertThrows(IllegalStateException.class,()->{
 		cidadeBefore.setNome("12312");
+		});
 	}
 
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_nome_contendo_caracteres_especiais() {
-		cidadeBefore.setNome("!@#!@!");
+	@Test
+	void test_nao_deve_settar_nome_contendo_caracteres_especiais() {
+		assertThrows(IllegalStateException.class,()->{
+			cidadeBefore.setNome("!@#!@!");
+		});
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_nome_contendo_menos_de_3_caracteres() {
+	@Test
+	void test_nao_deve_settar_nome_contendo_menos_de_3_caracteres() {
+		assertThrows(IllegalStateException.class,()->{
 		cidadeBefore.setNome("AR");
+		});
 	}
 	@Test
-	public void test_deve_settar_uf_valido() {
+	 void test_deve_settar_uf_valido() {
 		cidadeBefore.setUf(UF);
 		
 		assertSame(UF,cidadeBefore.getUf());
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_uf_com_quantidade_de_caracteres_a_mais() {
+	@Test
+	 void test_nao_deve_settar_uf_com_quantidade_de_caracteres_a_mais() {
+		assertThrows(IllegalStateException.class,()->{
 		cidadeBefore.setUf("PAA");
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_uf_com_quantidade_de_caracteres_a_menos() {
-		cidadeBefore.setUf("A");
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_uf_com_digitos() {
-		cidadeBefore.setUf("12");
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_nao_deve_settar_uf_nulo() {
-		cidadeBefore.setUf(null);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_settar_uf_vazio() {
-		cidadeBefore.setUf("");
+		});
+		
 	}
 	
 	@Test
-	public void test_deve_retornar_uf() {
+	 void test_nao_deve_settar_uf_com_quantidade_de_caracteres_a_menos() {
+		assertThrows(IllegalStateException.class,()->{
+		cidadeBefore.setUf("A");
+		});
+	}
+	
+	@Test
+	 void test_nao_deve_settar_uf_com_digitos() {
+		assertThrows(IllegalStateException.class,()->{
+		cidadeBefore.setUf("12");
+		});
+	}
+	
+	@Test
+	 void test_nao_deve_settar_uf_nulo() {
+		assertThrows(IllegalArgumentException.class,()->{
+		cidadeBefore.setUf(null);
+		});
+	}
+	
+	@Test
+	 void test_nao_deve_settar_uf_vazio() {
+		assertThrows(IllegalStateException.class,()->{
+		cidadeBefore.setUf("");
+		});
+		
+	}
+	
+	@Test
+	 void test_deve_retornar_uf() {
 		cidadeBefore.setUf(UF);
 		
 		assertSame(UF,cidadeBefore.getUf());
 	}
 	
-	@Test(expected = IllegalStateException.class)
-	public void test_nao_deve_csettaruf_contendo_caracteres_especiais() {
+	@Test
+	 void test_nao_deve_csettaruf_contendo_caracteres_especiais() {
+		assertThrows(IllegalStateException.class,()->{
 		cidadeBefore.setUf("!@");
+		});
 	}
 	
 	@Test
-	public void test_deve_retornar_igualdade_ao_comparar_a_cidade_com_a_mesma() {
+	 void test_deve_retornar_igualdade_ao_comparar_a_cidade_com_a_mesma() {
 		assertEquals(cidadeBefore,cidadeBefore);
 	}
 	
 	@Test
-	public void test_deve_retornar_igualdade_ao_comparar_objetos_com_os_mesmos_atributos_e_de_mesma_classe() {
+	 void test_deve_retornar_igualdade_ao_comparar_objetos_com_os_mesmos_atributos_e_de_mesma_classe() {
 		assertEquals(cidadeBefore,new Cidade(COD_IBGE));
 	}
 	
 	@Test
-	public void test_nao_deve_retornar_igualdade_ao_comparar_cidade_com_valor_null() {
+	 void test_nao_deve_retornar_igualdade_ao_comparar_cidade_com_valor_null() {
 		assertNotEquals(cidadeBefore,null);
 	}
 	
 	@Test
-	public void test_nao_deve_retornar_igualdade_de_objetos_de_classes_diferentes() {
+	 void test_nao_deve_retornar_igualdade_de_objetos_de_classes_diferentes() {
 		assertNotEquals(cidadeBefore,new Object());
 	}
 	
 	@Test
-	public void test_deve_retornar_igualdade_ao_comparar_o_hash_code_de_objetos_com_os_mesmos_atributos() {
+	 void test_deve_retornar_igualdade_ao_comparar_o_hash_code_de_objetos_com_os_mesmos_atributos() {
 		assertEquals(cidadeBefore.hashCode(), new Cidade(COD_IBGE).hashCode());
 	}
 	
 	@Test
-	public void test_nao_deve_retornar_igualdade_ao_comparar_hash_code_de_objetos_com_atributos_diferentes() {
+	 void test_nao_deve_retornar_igualdade_ao_comparar_hash_code_de_objetos_com_atributos_diferentes() {
 		assertNotEquals(cidadeBefore.hashCode(), new Cidade("12312").hashCode());
 	}
 	
 	@Test
-	public void test_deve_retornar_to_string_contendo_todos_os_atributos() {
+	 void test_deve_retornar_to_string_contendo_todos_os_atributos() {
 		cidadeBefore.setNome(NOME);
 		
 		cidadeBefore.setUf(UF);
